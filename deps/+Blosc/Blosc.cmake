e5bc0c8d15d87798232bf3ce12cad65c1cd3144e
Ludek Slouf <ludek.slouf@gmail.com>
2025-10-19 15:38:25 +0200

    BLocs CMAKE_POLICY_VERSION_MINIMUM=3.5

--- deps/+Blosc/Blosc.cmake
+++ deps/+Blosc/Blosc.cmake
@@ -16,6 +16,7 @@ add_cmake_project(Blosc
     URL https://github.com/Blosc/c-blosc/archive/8724c06e3da90f10986a253814af18ca081d8de0.zip
     URL_HASH SHA256=53986fd04210b3d94124b7967c857f9766353e576a69595a9393999e0712c035
     CMAKE_ARGS
+        -DCMAKE_POLICY_VERSION_MINIMUM=3.5 # <--- TENTO ŘÁDEK PŘIDAT!
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON
         -DBUILD_SHARED=${_build_shared}
         -DBUILD_STATIC=${_build_static}
